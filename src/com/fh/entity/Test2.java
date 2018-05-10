ecfe811285b513323d943db8cca1f869be78a998
michael <michael@DESKTOP-B2UGFTD>
2018-05-10 09:25:11 +0800

    test2 branch in test2 branch

---
+++ src/com/fh/entity/Test2.java
@@ -0,0 +1,20 @@
+/*
+ * 物融通公司源代码，版权归物融通公司所有。
+ * 
+ * 项目名称 : FHMYSQL
+ * 创建日期 : 2018年5月10日
+ * 修改历史 : 
+ *     1. [2018年5月10日]创建文件 by michael
+ */
+package com.fh.entity;
+
+/**
+ * //TODO 添加类/接口功能描述
+ * @author michael
+ * 2018年5月10日
+ */
+public class Test2 {
+public static void main(String[] args) {
+	System.err.println(456);
+}
+}
